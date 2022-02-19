3de1e2920ed2b18c05a2a4723d27fb8e1e3df2e9
Sarthak Verma <sarthakvdev@gmail.com>
2022-02-20 01:39:59 +0530

    setup for the contract done

---
+++ contracts/Domains.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+import "hardhat/console.sol";
+
+contract Domain {
+  constructor() {
+    console.log("This is my Domain's contract. Very nice.");
+  }
+}
